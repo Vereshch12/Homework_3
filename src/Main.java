0e8a1ae672df71c56477bf2303cd5610de0edd71
Vereshchagin_Mikhail <farasers@mail.ru>
2022-08-08 19:15:37 +0300

    Задание №4

--- src/Main.java
+++ src/Main.java
@@ -9,25 +9,25 @@ public class Main {
 
 
         //Задание №2
-        int YearOfIssue = 2014;
+        int yearOfIssue = 2014;
         if (clientOS == 0){
-            if (YearOfIssue >= 2015){
+            if (yearOfIssue >= 2015){
                 System.out.println("\nУстановите версию приложения для iOS по ссылке");
             } else
                 System.out.println("\nУстановите облегченную версию приложения для iOS по ссылке");
         } else{
-            if (YearOfIssue >= 2015){
+            if (yearOfIssue >= 2015){
                 System.out.println("\nУстановите версию приложения для Android по ссылке");
             } else
                 System.out.println("\nУстановите облегченную версию приложения для Android по ссылке");
         }
 
         //Задание №2 (вариант с else if)
-        if ((clientOS == 0) && (YearOfIssue >= 2015)){
+        if ((clientOS == 0) && (yearOfIssue >= 2015)){
                 System.out.println("\nУстановите версию приложения для iOS по ссылке");
-            } else if ((clientOS == 0) && (YearOfIssue < 2015)) {
+            } else if ((clientOS == 0) && (yearOfIssue < 2015)) {
                 System.out.println("\nУстановите облегченную версию приложения для iOS по ссылке");
-            } else if ((clientOS != 0) && (YearOfIssue >= 2015)){
+            } else if ((clientOS != 0) && (yearOfIssue >= 2015)){
                 System.out.println("\nУстановите облегченную версию приложения для Android по ссылке");
         } else{
                 System.out.println("\nУстановите облегченную версию приложения для Android по ссылке");
@@ -39,5 +39,15 @@ public class Main {
             System.out.println("\n" + year + " год високосный!");
         }else
             System.out.println("\n" + year + " год не високосный!");
+
+        //Задание №4
+        int deliveryDistance = 15;
+        int deliveryPeriod = 1;
+        if (deliveryDistance < 20){
+            System.out.println("\nПотребуется дней на доставку: " + deliveryPeriod);
+        }else {
+            deliveryPeriod += 1 + (deliveryDistance - 20) / 40;
+            System.out.println("\nПотребуется дней на доставку: " + deliveryPeriod);
+        }
     }
 }
