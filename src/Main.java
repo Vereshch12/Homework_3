2d4e74a7839f37f4e0cac5b062c07d52a66d2de7
Vereshchagin_Mikhail <farasers@mail.ru>
2022-08-08 18:57:57 +0300

    Задание №2
    Еще один вариант выполнения второго задания. Подскажите пожалуйста, здесь выполняются все условия?
    Или нужно просто отдельно 4 раза if писать?

--- src/Main.java
+++ src/Main.java
@@ -21,5 +21,16 @@ public class Main {
             } else
                 System.out.println("\nУстановите облегченную версию приложения для Android по ссылке");
         }
+
+        //Задание №2 (вариант с else if)
+        if ((clientOS == 0) && (YearOfIssue >= 2015)){
+                System.out.println("\nУстановите версию приложения для iOS по ссылке");
+            } else if ((clientOS == 0) && (YearOfIssue < 2015)) {
+                System.out.println("\nУстановите облегченную версию приложения для iOS по ссылке");
+            } else if ((clientOS != 0) && (YearOfIssue >= 2015)){
+                System.out.println("\nУстановите облегченную версию приложения для Android по ссылке");
+        } else{
+                System.out.println("\nУстановите облегченную версию приложения для Android по ссылке");
+        }
     }
 }
