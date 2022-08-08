8a8faf629ee8cde532c089d9b343bb345d06f022
Vereshchagin_Mikhail <farasers@mail.ru>
2022-08-08 18:33:51 +0300

    Задание №1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Задание №1
+        byte clientOS = 0;
+        if (clientOS == 0){
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else
+            System.out.println("Установите версию приложения для Android по ссылке");
     }
 }
